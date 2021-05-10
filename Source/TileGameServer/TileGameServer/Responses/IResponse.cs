a244a5a277d43fa5d1595bf2bf7c04f6eb1666ca
Artiom Ciobanu <aciobanumain@gmail.com>
2021-05-10 14:01:05 +0300

    Added IResponse.cs

---
+++ Source/TileGameServer/TileGameServer/Responses/IResponse.cs
@@ -0,0 +1,10 @@
+﻿using TileGameServer.Infrastructure.Enums;
+
+namespace TileGameServer.Responses
+{
+    public interface IResponse<out TResult>
+    {
+        TResult Result { get; }
+        ResponseStatus Status { get; }
+    }
+}
