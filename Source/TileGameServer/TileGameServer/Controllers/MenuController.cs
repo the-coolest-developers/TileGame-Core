555f5135b4199dbcedcc3b0ce698ddafb9fa0977
Artiom Ciobanu <aciobanumain@gmail.com>
2021-05-27 22:28:07 +0300

    Убрал TResponse из метода ExecuteAction

--- Source/TileGameServer/TileGameServer/Controllers/MenuController.cs
+++ Source/TileGameServer/TileGameServer/Controllers/MenuController.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TileGameServer.Infrastructure.Models.Dto.Responses.Menu;
 using TileGameServer.Services;
 
 namespace TileGameServer.Controllers
 {
     [ApiController]
-    public class MenuController : ControllerBase
+    [Route("menu")]
+    public class MenuController : BaseController
     {
         private IMenuService MenuService { get; }
 
         public MenuController(IMenuService menuService)
         {
             MenuService = menuService;
+        }
+
+        [HttpGet("createGame")]
+        public async Task<ActionResult<CreateGameResponse>> CreateGame()
+        {
+            return await ExecuteAction(() => MenuService.CreateGame(new Guid()));
         }
     }
 }
--- Source/TileGameServer/TileGameServer/Startup.cs
+++ Source/TileGameServer/TileGameServer/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using TileGameServer.Services;
 
 namespace TileGameServer
 {
@@ -19,8 +20,8 @@ namespace TileGameServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<IMenuService, MenuService>();
-            
+            services.AddScoped<IMenuService, MenuService>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
