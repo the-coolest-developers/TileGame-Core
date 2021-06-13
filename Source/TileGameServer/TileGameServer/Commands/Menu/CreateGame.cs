58d921d5917f3fd462fc1a3dd30f4d6389567b83
Artiom Ciobanu <aciobanumain@gmail.com>
2021-06-13 22:27:58 +0300

    [TG-38] Removed comments

--- Source/TileGameServer/TileGameServer/Commands/Menu/CreateGame.cs
+++ Source/TileGameServer/TileGameServer/Commands/Menu/CreateGame.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using TileGameServer.Hubs;
 using TileGameServer.Infrastructure.Enums;
 using TileGameServer.Infrastructure.Models.Dto.Responses.Generic;
 
@@ -16,6 +17,13 @@ namespace TileGameServer.Commands.Menu
 
         public class CreateGameCommandHandler : IRequestHandler<CreateGameCommand, CreateGameResponse>
         {
+            private MenuHub MenuHub { get; }
+
+            public CreateGameCommandHandler(MenuHub menuHub)
+            {
+                MenuHub = menuHub;
+            }
+
             public Task<CreateGameResponse> Handle(CreateGameCommand request, CancellationToken cancellationToken)
             {
                 return Task.FromResult(new CreateGameResponse
---
+++ Source/TileGameServer/TileGameServer/Repositories/ISessionRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TileGameServer.Enums;
+using TileGameServer.Models.Entities;
+
+namespace TileGameServer.Repositories
+{
+    public interface ISessionRepository
+    {
+        bool SessionExists(Guid gameId);
+        bool UserIsInSession(Guid userId);
+
+        SessionStatus GetSessionStatus(Guid gameId);
+        Session[] GetAllSessionsWithStatus(SessionStatus sessionStatus);
+        Session[] GetSessionWithId(Guid gameId);
+
+        void CloseSession(Guid gameId);
+        void AddUserToSession(Guid usedId);
+    }
+}
