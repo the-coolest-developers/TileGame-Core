62444240ccd5fbea06827e645b07112fcc8ec093
Artiom Ciobanu <aciobanumain@gmail.com>
2021-06-14 23:01:45 +0300

    [TG-38] Added Repository Interfaces and Entities

---
+++ Source/TileGameServer/TileGameServer.DataAccess/Repositories/Generic/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using TileGameServer.Infrastructure.Models.Entities;
+using TileGameServer.Infrastructure.Models.Entities.Base;
+
+namespace TileGameServer.DataAccess.Repositories.Generic
+{
+    public interface IRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        public Task CreateAsync(TEntity entity);
+        public Task UpdateAsync(TEntity entity);
+        public Task<TEntity> GetAsync(Guid id);
+        public Task DeleteAsync(Guid id);
+
+        public Task SaveChangesAsync();
+
+        public Task<bool> ExistsWithIdAsync(Guid id);
+    }
+}
