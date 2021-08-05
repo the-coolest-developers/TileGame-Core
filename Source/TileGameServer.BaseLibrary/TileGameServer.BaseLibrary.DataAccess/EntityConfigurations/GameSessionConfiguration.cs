6ca5c52c6d1520c83703cd537b4a898932125565
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-08-05 11:32:56 +0300

    [TGS-52] Added Player and GameSession configurations

---
+++ Source/TileGameServer.BaseLibrary/TileGameServer.BaseLibrary.DataAccess/EntityConfigurations/GameSessionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TileGameServer.BaseLibrary.Domain.Entities;
+using WebApiBaseLibrary.DataAccess.EntityConfigurations;
+
+namespace TileGameServer.BaseLibrary.DataAccess.EntityConfigurations
+{
+    public class GameSessionConfiguration : BaseEntityConfiguration<GameSession>
+    {
+        public override void Configure(EntityTypeBuilder<GameSession> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(gs => gs.Capacity).IsRequired();
+            builder.Property(gs => gs.Status).IsRequired();
+            builder.Property(gs => gs.CreationDate).IsRequired();
+
+            builder.HasMany(gs => gs.Players)
+                .WithOne(player => player.GameSession).IsRequired();
+        }
+    }
+}
---
+++ Source/TileGameServer.BaseLibrary/TileGameServer.BaseLibrary.DataAccess/EntityConfigurations/PlayerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TileGameServer.BaseLibrary.Domain.Entities;
+using WebApiBaseLibrary.DataAccess.EntityConfigurations;
+
+namespace TileGameServer.BaseLibrary.DataAccess.EntityConfigurations
+{
+    public class PlayerConfiguration : BaseEntityConfiguration<Player>
+    {
+        public override void Configure(EntityTypeBuilder<Player> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(player => player.GameSessionId).IsRequired();
+
+            builder.HasOne(player => player.GameSession)
+                .WithMany(gs => gs.Players).IsRequired();
+        }
+    }
+}
