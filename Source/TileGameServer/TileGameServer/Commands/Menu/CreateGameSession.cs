f160f1735ef712a8daa9160e0e5bf4d8f3d4bb45
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-08-05 11:40:15 +0300

    [TGS-52] ExistsWithPlayer renamed to ExistsWithPlayerInOpenSessions

--- Source/TileGameServer/TileGameServer/Commands/Menu/CreateGameSession.cs
+++ Source/TileGameServer/TileGameServer/Commands/Menu/CreateGameSession.cs
@@ -42,7 +42,7 @@ namespace TileGameServer.Commands.Menu
                     request.SessionCapacity >= _sessionCapacityConfigurator.Configuration.MinSessionCapacity
                     && request.SessionCapacity <= _sessionCapacityConfigurator.Configuration.MaxSessionCapacity;
 
-                if (_gameSessionsRepository.ExistsWithPlayer(request.AccountId) || !capacityIsValid)
+                if (await _gameSessionsRepository.ExistsWithPlayerInOpenSessionsAsync(request.AccountId) || !capacityIsValid)
                 {
                     return new Response<CreateGameSessionResponse>
                     {
