ab59926a9eb9554ee8bac670d321dde01e3dc44f
Artiom Ciobanu <aciobanumain@gmail.com>
2021-09-14 23:06:47 +0300

    [TGS-70] Added MessageQueueHostedService

---
+++ Source/TileGameServer.InSession/TileGameServer.InSession/HostedServices/MessageQueueHostedService.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing;
+
+namespace TileGameServer.InSession.HostedServices
+{
+    public class MessageQueueHostedService : IHostedService
+    {
+        private readonly IMessageQueueReader _joinGameReader;
+        private readonly IMessageQueueReader _leaveGameReader;
+
+        private readonly IMessageQueueConnection _connection;
+
+        public MessageQueueHostedService(IMessageQueueConnection connection)
+        {
+            _connection = connection;
+
+            _joinGameReader = connection.CreateReader("JoinGameQueue");
+            _joinGameReader.SetReceivedAction(JoinGameNotificationHandler);
+
+            _leaveGameReader = connection.CreateReader("LeaveGameQueue");
+            _leaveGameReader.SetReceivedAction(LeaveGameNotificationHandler);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _joinGameReader.StartReading();
+            _leaveGameReader.StartReading();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _connection.Dispose();
+
+            return Task.CompletedTask;
+        }
+
+        private void JoinGameNotificationHandler(string message)
+        {
+            Debug.WriteLine(" [x] Received {0}", message);
+        }
+
+        private void LeaveGameNotificationHandler(string message)
+        {
+            Debug.WriteLine(" [x] Received {0}", message);
+        }
+    }
+}
--- Source/TileGameServer.InSession/TileGameServer.InSession/Startup.cs
+++ Source/TileGameServer.InSession/TileGameServer.InSession/Startup.cs
@@ -7,20 +7,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TileGameServer.InSession.Constants;
+using TileGameServer.InSession.HostedServices;
+using WebApiBaseLibrary.Infrastructure.Configuration;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ.Extensions;
 
 namespace TileGameServer.InSession
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IServiceProvider _serviceProvider;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var rabbitMqConfiguration = new RabbitMQConfiguration
+            {
+                HostName = Environment.GetEnvironmentVariable(EnvironmentVariables.RabbitMQHostName)
+            };
+
+            services.AddRabbitMQ(rabbitMqConfiguration, () => _serviceProvider);
+
+            services.AddHostedService<MessageQueueHostedService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            _serviceProvider = app.ApplicationServices;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
