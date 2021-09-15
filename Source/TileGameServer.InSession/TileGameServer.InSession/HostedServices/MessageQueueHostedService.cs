00caec298c1ebf00e755c4b829a996e228a585d8
Artiom Ciobanu <aciobanumain@gmail.com>
2021-09-15 23:25:12 +0300

    [TGS-70] Message serialized to notification class

--- Source/TileGameServer.InSession/TileGameServer.InSession/HostedServices/MessageQueueHostedService.cs
+++ Source/TileGameServer.InSession/TileGameServer.InSession/HostedServices/MessageQueueHostedService.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using TileGameServer.BaseLibrary.Domain.MessageQueueNotifications;
 using WebApiBaseLibrary.Infrastructure.MessageQueueing;
 
 namespace TileGameServer.InSession.HostedServices
@@ -37,12 +39,16 @@ namespace TileGameServer.InSession.HostedServices
 
         private void JoinGameNotificationHandler(string message)
         {
-            Debug.WriteLine($"A player has joined the game: {message}");
+            var joinGameNotification = JsonConvert.DeserializeObject<JoinGameNotification>(message);
+
+            Debug.WriteLine($"A player has joined the game: {joinGameNotification.PlayerNickname}");
         }
 
         private void LeaveGameNotificationHandler(string message)
         {
-            Debug.WriteLine($"A player has left the game: {message}");
+            var leaveGameNotification = JsonConvert.DeserializeObject<LeaveGameNotification>(message);
+
+            Debug.WriteLine($"A player has left the game: {leaveGameNotification.PlayerId}");
         }
     }
 }
